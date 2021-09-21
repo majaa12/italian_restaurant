a5d3695f53b837ee94325ff79c7f64b2cd83f026
majaa12 <majacol98@gmail.com>
2021-09-21 03:03:13 +0200

    Dodat lokalni storage

--- src/pages/MenuPage/MenuPage.jsx
+++ src/pages/MenuPage/MenuPage.jsx
@@ -1,14 +1,30 @@
-import { useState } from "react";
+import { useState, useEffect} from "react";
 
 import MenuCard from "../../components/MenuCard/MenuCard";
 import Order from "../../components/Order/Order";
 import menu from "../../data/menu";
 import "./MenuPage.css";
 
+const LOCAL_STORAGE_KEY = "react-order-items";
+
 const MenuPage = () => {
   const [orderItems, setOrderItems] = useState([]);
   const [orderItemId, setOrderItemId] = useState(0);
 
+  useEffect(()=>{
+    const storageOrderItems = JSON.parse(localStorage.getItem(LOCAL_STORAGE_KEY));
+    if(storageOrderItems){
+      setOrderItems(storageOrderItems);
+    }
+  }, []);
+
+
+  useEffect( () =>{
+    localStorage.setItem(LOCAL_STORAGE_KEY, JSON.stringify(orderItems));
+  }, [orderItems]);
+
+
+  
   const addToOrder = (item) => {
     const updatedItems = [...orderItems, { ...item, id: orderItemId }];
     setOrderItems(updatedItems);
